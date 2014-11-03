7a25b3dddf51b8d891368804d8ce9c326b457cc3
vamsi1401 <vamsi1401@gmail.com>
2014-11-03 12:43:10 +0530

    01/11/14

--- code-basic/demo.m
+++ code-basic/demo.m
@@ -18,20 +18,37 @@ disp('model tree visualization');
 disp('press any key to continue'); 
 pause;
 
-imlist = {};%dir('images/*.jpg');
-for i = 1%:length(imlist)
+%imlist = dir('C:/Users/Vamsi/Documents/GitHub/ddp_fmop/code-full/TEST/*.jpg');
+for i = 1:2000%length(imlist)
     % load and display image
-    im = imread('seq_002000.jpg');
+    im = imread(sprintf('C:/Users/Vamsi/Documents/GitHub/ddp_fmop/code-full/TEST/seq_%0.6d.jpg',i));
     clf; imagesc(im); axis image; axis off; drawnow;
 
     % call detect function
     tic;
     boxes = detect_fast(im, model, min(model.thresh,-1));
     dettime = toc; % record cpu time
-    boxes = nms(boxes, .1); % nonmaximal suppression
+    bb_max_height = 45.0 ;
+    bb_min_height = 13.0 ;
+    %bb_max_width = 65.0 ;
+    %bb_min_width = 15.0 ;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    i2 = 1 ;
+    for i1 = 1:length(boxes)
+        a = (boxes(i1,2) + boxes(i1,4))/2.0 ;
+        height = ((bb_max_height - bb_min_height)*a)/479.0 + bb_min_height ;
+        %width = ((bb_max_width - bb_min_width)*a)/479.0 + bb_min_width ;
+        h1 = boxes(i1,4) - boxes(i1,2) ;
+        %w1 = boxes(i1,1) - boxes(i1,3) ;
+        if(h1/height < 1.1 && h1/height > 0.9)
+            boxes1(i2,:) = boxes(i1,:);
+            i2 = i2 +1 ;
+        end 
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    boxes = nms(boxes1, .2); % nonmaximal suppression
     colorset = {'g','y'};
-    showboxes(im, boxes(1:8,:
-    ),colorset); % show the best detection
+    showboxes(im, boxes(:,:),colorset); % show the best detection
     %showboxes(im, boxes,colorset);  % show all detections
     fprintf('detection took %.1f seconds\n',dettime);
     disp('press any key to continue');
